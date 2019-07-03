757a731485d1f5dfa424851cef2da8f831511168
mijkami <dimitri.gaillot@gmail.com>
2019-07-03 11:10:36 +0200

    update sortieEdit

--- resources/views/pages/sortieEdit.blade.php
+++ resources/views/pages/sortieEdit.blade.php
@@ -5,15 +5,19 @@
 <a href="/sors">Retour arrière</a><br>
 {{-- store => update --}}
 
-<form class="form-horizontal" method="POST" action="{{ route("sors.store") }}">
-        @csrf
+<form class="form-horizontal" method="POST" action="{{ route("sors.update", $sor->id) }}">
+    {{ csrf_field() }}
+
+    {{-- définition de la méthode --}}
+    {{ method_field('PuT') }}
         <input type="date" name="dat" value="{{ $sor->dat }}"> Date<br>
+        <input type="radio" name="typ" value="sortie normale" <?php if ($sor->typ=="sortie normale"){echo 'checked';} ?>> Sortie normale<br>
         <input type="radio" name="typ" value="montagne" <?php if ($sor->typ=="montagne"){echo 'checked';} ?>> Montagne<br>
         <input type="radio" name="typ" value="encadrée" <?php if ($sor->typ=="encadrée"){echo 'checked';} ?>> Encadrée<br>
         <input type="radio" name="typ" value="1500" <?php if ($sor->typ=="1500"){echo 'checked';} ?>> 1500<br>
         <input type="radio" name="typ" value="accompagnée" <?php if ($sor->typ=="accompagnée"){echo 'checked';} ?>> Accompagnée<br>
         <input type="radio" name="typ" value="sunset" <?php if ($sor->typ=="sunset"){echo 'checked';} ?>> Sunset<br>
-        <textarea rows="4" cols="50" name="comment_sor" value="{{ $sor->comment_sor }}"> Commentaire...</textarea><br>
+<textarea rows="4" cols="50" name="comment_sor" value="{{ $sor->comment_sor }}"> {{ $sor->comment_sor }}</textarea><br>
 
         {{-- <input type="textarea"  name="comment_sor"> Commentaire<br> --}}
         <button type="submit">Enregistrer</button>
