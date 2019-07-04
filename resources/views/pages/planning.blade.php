7c222264e8e6ab64200dce15396ca2a6981c81a8
mijkami <dimitri.gaillot@gmail.com>
2019-07-04 11:26:51 +0200

    add user count + edit/delete icons

--- resources/views/pages/planning.blade.php
+++ resources/views/pages/planning.blade.php
@@ -17,14 +17,15 @@
         echo Date::parse($sor->dat)->format('l j F')." , ".$sor->typ."<br>";
         echo $sor->comment_sor."<br>";
         $participSor = $particips->Where('sor_id','=', $sor->id);
+        $participNum=0;
         foreach ($participSor as $particip) {
             // ajouter IF pour afficher différemment selon admin ou autre utilisateur,
             // tester si le $particip->user_id correspond à la session de id
             if (session('role')=='admin' or session('role')=='superadmin' or (session('role')=='membre' and session('id')==$particip->user_id)){
                 // liens de suppression et de modification;
-                echo '<a href="/particips/'.$particip->id.'/edit">éditer</a>'.' <a href="/particips/'.$particip->id.'/destroy">détruire</a><br>';
+                echo '<a href="/particips/'.$particip->id.'/destroy"><i class="fas fa-user-times"></i></a> '.'<a href="/particips/'.$particip->id.'/edit"><i class="fas fa-user-edit"></i></a> ';
             }
-            echo $particip->User->name.'<br>';
+            echo ++$participNum.". ".$particip->User->name.'<br>';
         }
         echo "<br>________<br><br>";
     }
