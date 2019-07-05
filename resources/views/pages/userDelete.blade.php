36b3ead9c9f88c11f3fa0201b0b06e2a7fed54b4
mijkami <dimitri.gaillot@gmail.com>
2019-07-05 12:22:07 +0200

    user Create/Delete/Edit only shown to superadmin

--- resources/views/pages/userDelete.blade.php
+++ resources/views/pages/userDelete.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app')
 @section('content')
+@if (session('role')=='superadmin')
+    <h1>Suppression de {{ $user->name }}</h1>
+    <a href="/users">Retour arrière</a><br>
 
-<h1>Suppression de {{ $user->name }}</h1>
-<a href="/users">Retour arrière</a><br>
+    {{-- store => update --}}
 
-{{-- store => update --}}
+    <form  method="POST" action="{{ route('users.destroy', $user->id) }}">
+        {{-- protection --}}
+        {{ csrf_field() }}
 
-<form  method="POST" action="{{ route('users.destroy', $user->id) }}">
-    {{-- protection --}}
-    {{ csrf_field() }}
-
-    {{-- définition de la méthode --}}
-    {{ method_field('DELETE') }}
-    <button type="submit">Supprimer</button>
-    {{-- possibilité d'ajouter une confirmation supplémentaire cf JS/CSS, etc --}}
-</form>
-
+        {{-- définition de la méthode --}}
+        {{ method_field('DELETE') }}
+        <button type="submit">Supprimer</button>
+        {{-- possibilité d'ajouter une confirmation supplémentaire cf JS/CSS, etc --}}
+    </form>
+@endif
 
 @stop
