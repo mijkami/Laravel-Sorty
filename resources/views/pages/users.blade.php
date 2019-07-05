4f05158a205801c7ee84dba446bb4693640b03d6
mijkami <dimitri.gaillot@gmail.com>
2019-07-05 17:51:16 +0200

    modif Users view presentation

--- resources/views/pages/users.blade.php
+++ resources/views/pages/users.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     @if (session('role')=='admin')
-        <h1>Liste d'utilisateurs</h1>
+        <h2>Liste d'utilisateurs</h2>
             <?php
                 foreach ($users as $user ) {
                     echo '<div class="row justify-content-start no-gutters mt-4 mt-md-0"><div class="col-5 col-md-2">'.$user->name.'</div><div class="col-4 col-md-2">'.$user->firstname.'</div><div class="col-4 col-md-3 ">'.$user->tel.'</div><div class="col-5 col-md-4">'.$user->email.'</div></div>';
@@ -10,12 +10,12 @@
             ?>
     @endif
     @if (session('role')=='superadmin')
-        <h1>Liste d'utilisateurs</h1>
+        <h2>Liste d'utilisateurs</h2>
         <h3 class="ml-4"><a href="/users/create">Créer un utilisateur</a></h3>
             <?php
 
                 foreach ($users as $user ) {
-                    echo '<div class="row justify-content-start no-gutters mt-4 mt-md-0"><div class="col-2 col-md-1"><a href="/users/'.$user->id.'/edit"><i class="fas fa-user-edit"></i></a> <a href="/users/'.$user->id.'/destroy"><i class="fas fa-user-times"></i></a></div><div class="col-5 col-md-2">'.$user->name.'</div><div class="col-4 col-md-2">'.$user->firstname.'</div><div class="col-4 offset-2 col-md-3 offset-md-0">'.$user->tel.'</div><div class="col-5 col-md-4">'.$user->email.'</div></div>';
+                    echo '<div class="row justify-content-start no-gutters mt-4 mt-md-0"><div class="col-2 col-md-1"><a href="/users/'.$user->id.'/edit"><i class="fas fa-user-edit"></i></a> / <a href="/users/'.$user->id.'/destroy"><i class="fas fa-user-times"></i></a></div><div class="col-5 col-md-2">'.$user->name.'</div><div class="col-4 col-md-2">'.$user->firstname.'</div><div class="col-4 offset-2 col-md-3 offset-md-0">'.$user->tel.'</div><div class="col-5 col-md-4">'.$user->email.'</div></div>';
                 }
             ?>
     @endif
