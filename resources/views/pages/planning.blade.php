2b0398c787ecf4de481da2e39b70a93a0a52bcec
mijkami <dimitri.gaillot@gmail.com>
2019-07-05 12:18:49 +0200

    responsive planning

--- resources/views/pages/planning.blade.php
+++ resources/views/pages/planning.blade.php
@@ -1,33 +1,37 @@
 @extends('layouts.app')
 @section('content')
 
-<h1>Sorties prochaines & Participants</h1><br>
+<h3>Sorties prochaines & Participants</h3>
 <?php
     session(['page' => "/particips"]);
     if (session('role')=='admin' or session('role')=='superadmin'){
-        echo '<h4><a href="/particips/create2">Création de participation (mode administrateur)</a></h4>';
+        echo '<a href="/particips/create2"><p>Création de participation (mode administrateur)</p></a>';
     } elseif (session('role')=='membre'){
-        echo '<h4><a href="/particips/create">Création de participation</a></h4>';
+        echo '<a href="/particips/create"><p>Création de participation</p></a>';
     }
 
 
     //affichage des sorties
+
     $sorFutur = $sors->Where('dat', '>=', today());
     foreach ($sorFutur as $sor) {
-        echo Date::parse($sor->dat)->format('l j F')." , ".$sor->typ."<br>";
-        echo $sor->comment_sor."<br>";
+        echo "<div class='mb-5'><p class='font-weight-bold mb-0'>".Date::parse($sor->dat)->format('l j F')." , ".$sor->typ."</p>";
+        echo "<p class='col col-lg-7 p-0 mb-2'>".$sor->comment_sor."</p>";
         $participSor = $particips->Where('sor_id','=', $sor->id);
         $participNum=0;
+
         foreach ($participSor as $particip) {
             // ajouter IF pour afficher différemment selon admin ou autre utilisateur,
             // tester si le $particip->user_id correspond à la session de id
+            echo '<div class="row no-gutters ml-2">';
             if (session('role')=='admin' or session('role')=='superadmin' or (session('role')=='membre' and session('id')==$particip->user_id)){
                 // liens de suppression et de modification;
-                echo '<a href="/particips/'.$particip->id.'/destroy"><i class="fas fa-user-times"></i></a> '.'<a href="/particips/'.$particip->id.'/edit"><i class="fas fa-user-edit"></i></a> ';
+                echo '<div class="col-2 col-md-1"><a href="/particips/'.$particip->id.'/destroy"><i class="fas fa-user-times"></i></a> '.'<a href="/particips/'.$particip->id.'/edit"><i class="fas fa-user-edit"></i></a></div> ';
             }
-            echo ++$participNum.". ".$particip->User->name.'<br>';
+            //
+            echo '<div class="col-5 col-sm-4 col-md-2">'.++$participNum.". ".$particip->User->firstname." ".$particip->User->name."</div><div class='col-5 col-sm-4 col-md-5 '>".$particip->comment_particip.'</div><div class="col-4 col-md-4"></div></div>';
         }
-        echo "<br>________<br><br>";
+        echo "</div>";
     }
 
 
