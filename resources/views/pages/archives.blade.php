7265a57159cdb38ff721861d755d70339b92fb66
mijkami <dimitri.gaillot@gmail.com>
2019-07-05 17:52:08 +0200

    adding edit/dele links in Archives for admins

--- resources/views/pages/archives.blade.php
+++ resources/views/pages/archives.blade.php
@@ -7,7 +7,11 @@
     $sorPasse = $sors->Where('dat', '<', today());
 
     foreach ($sorPasse as $sor) {
-        echo "<div class='mt-3'><p class='font-weight-bold mb-0'>".Date::parse($sor->dat)->format('l j F')." , ".$sor->typ."</p>";
+        echo "<div class='mt-3'><p class='font-weight-bold mb-0'>".Date::parse($sor->dat)->format('l j F')." , ".$sor->typ;
+            if (session('role')=='admin' or session('role')=='superadmin'){
+            echo ' | <a href="/sors/'.$sor->id.'/edit"><i class="far fa-edit"> Éditer</i></a> '.'/ <a href="/sors/'.$sor->id.'/destroy"><i class="far fa-times"> Effacer</i></a>';
+        }
+        echo "</p>";
         echo "<p class='col col-lg-7 p-0 mb-2'>".$sor->comment_sor."</p>";
         $archivesPlanning = $archives->Where('sor_id','=', $sor->id);
         $participNum=0;
@@ -15,7 +19,7 @@
             echo '<div class="row no-gutters ml-2">';
 
             if (session('role')=='admin' OR session('role')=='superadmin'){
-                echo '<div class="col-2 col-md-1"><a href="/particips/'.$archives->id.'/destroy"><i class="fas fa-user-times"></i></a></div>';
+                echo '<div class="col-2 col-md-1"><a href="/particips/'.$archives->id.'/destroy"><i class="fas fa-user-times"></i></a> / <a href="/particips/'.$archives->id.'/edit"><i class="fas fa-user-edit"></i></a></div>';
             }
             echo '<div class="col-5 col-sm-4 col-md-2">'.++$participNum.". ".$archives->User->firstname." ".$archives->User->name."</div><div class='col-5 col-sm-4 col-md-5 '></div></div>";
 
