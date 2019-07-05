a3b0f7c932064da3f5ee0b711d073137899d2346
mijkami <dimitri.gaillot@gmail.com>
2019-07-05 17:01:57 +0200

    Add admin-only access : sortieDelete & sortieEdit

--- resources/views/pages/sortieDelete.blade.php
+++ resources/views/pages/sortieDelete.blade.php
@@ -1,20 +1,19 @@
 @extends('layouts.app')
 @section('content')
+@if (session('role')=='admin' OR session('role')=='superadmin')
+    <h3>Suppression de la sortie {{ $sor->typ . ' du ' . Date::parse($sor->dat)->format('l j F')}}</h3>
+    <a href="/sors">Retour arrière</a><br>
 
+    {{-- store => update --}}
 
-<h3>Suppression de la sortie {{ $sor->typ . ' du ' . Date::parse($sor->dat)->format('l j F')}}</h3>
-<a href="/sors">Retour arrière</a><br>
+    <form  method="POST" action="{{ route('sors.destroy', $sor->id) }}">
+        {{-- protection --}}
+        {{ csrf_field() }}
 
-{{-- store => update --}}
-
-<form  method="POST" action="{{ route('sors.destroy', $sor->id) }}">
-    {{-- protection --}}
-    {{ csrf_field() }}
-
-    {{-- définition de la méthode --}}
-    {{ method_field('DELETE') }}
-    <button type="submit">Supprimer</button>
-    {{-- possibilité d'ajouter une confirmation supplémentaire cf JS/CSS, etc --}}
-</form>
-
+        {{-- définition de la méthode --}}
+        {{ method_field('DELETE') }}
+        <button type="submit">Supprimer</button>
+        {{-- possibilité d'ajouter une confirmation supplémentaire cf JS/CSS, etc --}}
+    </form>
+@endif
 @stop
--- resources/views/pages/sortieEdit.blade.php
+++ resources/views/pages/sortieEdit.blade.php
@@ -1,27 +1,27 @@
 @extends('layouts.app')
 @section('content')
+@if (session('role')=='admin' OR session('role')=='superadmin')
+    <h1>Edition de sortie</h1><br>
+    <a href="/sors">Retour arrière</a><br>
+    {{-- store => update --}}
 
-<h1>Edition de sortie</h1><br>
-<a href="/sors">Retour arrière</a><br>
-{{-- store => update --}}
+    <form class="form-horizontal" method="POST" action="{{ route("sors.update", $sor->id) }}">
+        {{ csrf_field() }}
 
-<form class="form-horizontal" method="POST" action="{{ route("sors.update", $sor->id) }}">
-    {{ csrf_field() }}
+        {{-- définition de la méthode --}}
+        {{ method_field('PuT') }}
+            <input type="date" name="dat" value="{{ $sor->dat }}"> Date<br>
+            <input type="radio" name="typ" value="sortie normale" <?php if ($sor->typ=="sortie normale"){echo 'checked';} ?>> Sortie normale<br>
+            <input type="radio" name="typ" value="montagne" <?php if ($sor->typ=="montagne"){echo 'checked';} ?>> Montagne<br>
+            <input type="radio" name="typ" value="encadrée" <?php if ($sor->typ=="encadrée"){echo 'checked';} ?>> Encadrée<br>
+            <input type="radio" name="typ" value="1500" <?php if ($sor->typ=="1500"){echo 'checked';} ?>> 1500<br>
+            <input type="radio" name="typ" value="accompagnée" <?php if ($sor->typ=="accompagnée"){echo 'checked';} ?>> Accompagnée<br>
+            <input type="radio" name="typ" value="sunset" <?php if ($sor->typ=="sunset"){echo 'checked';} ?>> Sunset<br>
+    <textarea rows="4" cols="50" name="comment_sor" value="{{ $sor->comment_sor }}"> {{ $sor->comment_sor }}</textarea><br>
 
-    {{-- définition de la méthode --}}
-    {{ method_field('PuT') }}
-        <input type="date" name="dat" value="{{ $sor->dat }}"> Date<br>
-        <input type="radio" name="typ" value="sortie normale" <?php if ($sor->typ=="sortie normale"){echo 'checked';} ?>> Sortie normale<br>
-        <input type="radio" name="typ" value="montagne" <?php if ($sor->typ=="montagne"){echo 'checked';} ?>> Montagne<br>
-        <input type="radio" name="typ" value="encadrée" <?php if ($sor->typ=="encadrée"){echo 'checked';} ?>> Encadrée<br>
-        <input type="radio" name="typ" value="1500" <?php if ($sor->typ=="1500"){echo 'checked';} ?>> 1500<br>
-        <input type="radio" name="typ" value="accompagnée" <?php if ($sor->typ=="accompagnée"){echo 'checked';} ?>> Accompagnée<br>
-        <input type="radio" name="typ" value="sunset" <?php if ($sor->typ=="sunset"){echo 'checked';} ?>> Sunset<br>
-<textarea rows="4" cols="50" name="comment_sor" value="{{ $sor->comment_sor }}"> {{ $sor->comment_sor }}</textarea><br>
-
-        {{-- <input type="textarea"  name="comment_sor"> Commentaire<br> --}}
-        <button type="submit">Enregistrer</button>
-</form>
-
+            {{-- <input type="textarea"  name="comment_sor"> Commentaire<br> --}}
+            <button type="submit">Enregistrer</button>
+    </form>
+@endif
 @stop
 
