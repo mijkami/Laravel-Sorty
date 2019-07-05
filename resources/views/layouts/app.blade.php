b2883fb1a54d7a56c3dc7910c912e848ea016437
mijkami <dimitri.gaillot@gmail.com>
2019-07-05 13:28:29 +0200

    clean-up <main> in layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,14 +92,10 @@
         <main class="py-4">
             @include('flash-message')
             <div class="container">
-                <div class="row">
-                    <div class="col-md-12 col-md-offset-2">
-                        <div class="panel panel-default">
-                            <div class="panel-heading"></div>
-                            <div class="panel-body">
-                            {{-- content --}}
-                                @yield('content')
-                            </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading"></div>
+                        <div class="panel-body">
+                            @yield('content')
                         </div>
                     </div>
                 </div>
