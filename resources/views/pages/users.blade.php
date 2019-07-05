073471edeb0bf8934ba2ce658969954c8dfe414e
mijkami <dimitri.gaillot@gmail.com>
2019-07-05 12:19:35 +0200

    responsive users page

--- resources/views/pages/users.blade.php
+++ resources/views/pages/users.blade.php
@@ -1,14 +1,24 @@
 @extends('layouts.app')
 @section('content')
+    @if (session('role')=='admin')
+        <h1>Liste d'utilisateurs</h1>
+            <?php
+                foreach ($users as $user ) {
+                    echo '<div class="row justify-content-start no-gutters mt-4 mt-md-0"><div class="col-5 col-md-2">'.$user->name.'</div><div class="col-4 col-md-2">'.$user->firstname.'</div><div class="col-4 col-md-3 ">'.$user->tel.'</div><div class="col-5 col-md-4">'.$user->email.'</div></div>';
 
-<h1>Liste d'utilisateurs</h1>
-<h4><a href="/users/create">Créer un utilisateur</a></h4>
-    <?php
+                }
+            ?>
+    @endif
+    @if (session('role')=='superadmin')
+        <h1>Liste d'utilisateurs</h1>
+        <h3 class="ml-4"><a href="/users/create">Créer un utilisateur</a></h3>
+            <?php
 
-        foreach ($users as $user ) {
-            echo "<span>".$user->name."</span>".' <a href="/users/'.$user->id.'/edit">      éditer</a>'.' <a href="/users/'.$user->id.'/destroy">      Détruire</a><br>';
-        }
-    ?>
+                foreach ($users as $user ) {
+                    echo '<div class="row justify-content-start no-gutters mt-4 mt-md-0"><div class="col-2 col-md-1"><a href="/users/'.$user->id.'/edit"><i class="fas fa-user-edit"></i></a> <a href="/users/'.$user->id.'/destroy"><i class="fas fa-user-times"></i></a></div><div class="col-5 col-md-2">'.$user->name.'</div><div class="col-4 col-md-2">'.$user->firstname.'</div><div class="col-4 offset-2 col-md-3 offset-md-0">'.$user->tel.'</div><div class="col-5 col-md-4">'.$user->email.'</div></div>';
+                }
+            ?>
+    @endif
 
 
 @stop
