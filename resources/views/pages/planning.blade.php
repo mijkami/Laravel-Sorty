d27ad691c6d9117479173be7e4cc1984eb461ed2
mijkami <dimitri.gaillot@gmail.com>
2019-07-08 17:11:54 +0200

    Add waiting list feature in planning + admin links

--- resources/views/pages/planning.blade.php
+++ resources/views/pages/planning.blade.php
@@ -3,12 +3,9 @@
 <h2>Sorties prochaines & Participants</h2>
 <?php
     session(['page' => "/particips"]);
-    if (session('role')=='superadmin'){
+    if (session('role')=='admin' or session('role')=='superadmin'){
         echo '<a href="/sors/create"><p><i class="fas fa-shuttle-van"> Créer une sortie</i></p></a>';
         echo '<a href="/particips/create2"><p><i class="fa fa-plus"></i> Création de participation (mode administrateur)</p></a>';
-    }
-    elseif (session('role')=='admin'){
-        echo '<a href="/particips/create2"><p><i class="fas fa-plus"></i> Création de participation (mode administrateur)</p></a>';
     }
     elseif (session('role')=='membre'){
         echo '<a href="/particips/create"><p><i class="fas fa-plus"> Création de participation</i></p></a>';
@@ -31,6 +28,9 @@
         foreach ($participSor as $particip) {
             // ajouter IF pour afficher différemment selon admin ou autre utilisateur,
             // tester si le $particip->user_id correspond à la session de id
+            if ($participNum==8){
+                echo "<font color='#00008B'><span class='row ml-5'>Liste d'attente :</span>";
+            }
             echo '<div class="row justify-content-end no-gutters ml-2">';
             if (session('role')=='admin' or session('role')=='superadmin' or (session('role')=='membre' and session('id')==$particip->user_id)){
                 echo '<div class="col-2 col-sm-2 col-md-1"><a href="/particips/'.$particip->id.'/destroy"><i class="fas fa-user-times"></i></a> / <a href="/particips/'.$particip->id.'/edit"><i class="fas fa-user-edit"></i></a></div> ';
@@ -41,18 +41,18 @@
             if (session('role')=='admin' or session('role')=='superadmin' or (session('role')=='membre')){
                 echo $particip->User->tel;
            }
-            echo '</div><div class="col-4 col-sm-2 col-md-2">';
+            echo '</div><div class="col-4 col-sm-2 col-md-1">';
             if (session('role')=='admin' or session('role')=='superadmin'){
                 echo Date::parse($particip->created_at)->format('j F');
             }
-            echo '</div><div class="col-10 col-sm-10 col-md-3 col-lg-5">';
+            echo '</div><div class="col-10 col-sm-10 col-md-3 col-lg-6">';
             if (session('role')=='admin' or session('role')=='superadmin' or (session('role')=='membre')){
                 echo $particip->comment_particip;
             }
             echo '</div></div>';
 
         }
-        echo "</section>";
+        echo "</font></section>";
     }
 
 
