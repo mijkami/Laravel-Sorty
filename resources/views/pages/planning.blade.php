6da4549308b3bce9cd99da5f7a2d251b1c8bd40d
mijkami <dimitri.gaillot@gmail.com>
2019-07-08 12:51:37 +0200

    updating planning responsive

--- resources/views/pages/planning.blade.php
+++ resources/views/pages/planning.blade.php
@@ -33,19 +33,19 @@
             // tester si le $particip->user_id correspond à la session de id
             echo '<div class="row justify-content-end no-gutters ml-2">';
             if (session('role')=='admin' or session('role')=='superadmin' or (session('role')=='membre' and session('id')==$particip->user_id)){
-                echo '<div class="col-2 col-md-1"><a href="/particips/'.$particip->id.'/destroy"><i class="fas fa-user-times"></i></a> / <a href="/particips/'.$particip->id.'/edit"><i class="fas fa-user-edit"></i></a></div> ';
+                echo '<div class="col-2 col-sm-2 col-md-1"><a href="/particips/'.$particip->id.'/destroy"><i class="fas fa-user-times"></i></a> / <a href="/particips/'.$particip->id.'/edit"><i class="fas fa-user-edit"></i></a></div> ';
             }
             //
-            echo '<div class="col-5 col-sm-4 col-md-2">'.++$participNum.'. '.$particip->User->firstname.' '.$particip->User->name.'</div>';
-            echo '<div class="col-2 col-md-2">';
+            echo '<div class="col-10 col-sm-4 col-md-3 col-lg-2">'.++$participNum.'. '.$particip->User->firstname.' '.$particip->User->name.'</div>';
+            echo '<div class="col-6 col-sm-4 col-md-3 col-lg-2">';
             if (session('role')=='admin' or session('role')=='superadmin' or (session('role')=='membre')){
                 echo $particip->User->tel;
            }
-            echo '</div><div class="col-2 col-md-2">';
+            echo '</div><div class="col-4 col-sm-2 col-md-2">';
             if (session('role')=='admin' or session('role')=='superadmin'){
                 echo Date::parse($particip->created_at)->format('j F');
             }
-            echo '</div><div class="col-5 col-sm-4 col-md-5">';
+            echo '</div><div class="col-10 col-sm-10 col-md-3 col-lg-5">';
             if (session('role')=='admin' or session('role')=='superadmin' or (session('role')=='membre')){
                 echo $particip->comment_particip;
             }
