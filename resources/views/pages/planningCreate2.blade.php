475633089d6f44a44baaa34731f395d674ac42a7
mijkami <dimitri.gaillot@gmail.com>
2019-07-08 09:55:42 +0200

    fixing mis-placed code planningCreate2

--- resources/views/pages/planningCreate2.blade.php
+++ resources/views/pages/planningCreate2.blade.php
@@ -2,31 +2,22 @@
 @section('content')
 
 <h1>Nouvelle participation (mode admin)</h1><br>
-{{-- ajouter "Nouvelle participation (mode admin)" --}}
 <a href="/particips">Retour arrière</a><br>
-
-{{-- TODO : ajouter champ select avec la liste de tous les utilisateurs par ordre alphabétique --}}
-
-<select name="sor_id" size=10>
-        <?Php
-            foreach ($users as $user){
-                    echo '<option value="'.$user->id.'"';
-                    if ($user->id==session('id')){
-                        echo 'selected="selected"';
-                    }
-                    echo '>'.$user->name." ".$user->firstname.'</option>';
-
-
-            }
-        ?>
-</select><br>
-
-
-
 <form class="form-horizontal" method="POST" action="{{ route("particips.store") }}">
      @csrf
-    <input type="hidden" name="user_id" value={{session('id')}}>
+    <select name="user_id" size=10>
+            <?Php
+                foreach ($users as $user){
+                        echo '<option value="'.$user->id.'"';
+                        if ($user->id==session('id')){
+                            echo 'selected="selected"';
+                        }
+                        echo '>'.$user->name." ".$user->firstname.'</option>';
 
+
+                }
+            ?>
+    </select><br>
     <select name="sor_id" size=5>
         <?Php
             foreach ($sorFutur as $sor){
