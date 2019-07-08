a345bd1de5c0419ca7d647763473a20c4f384b87
mijkami <dimitri.gaillot@gmail.com>
2019-07-08 12:51:25 +0200

    updating tinyMCE

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,8 +29,6 @@
         plugins: ' link ',
         toolbar: 'bold italic underline forecolor  fontsizeselect | alignleft aligncenter alignright | link removeformat |',
         forced_root_block : false,
-        force_br_newlines : true,
-        force_p_newlines : false,
         });
     </script>
 </head>
