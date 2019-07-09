1bdcb7086cfd109f285184010f2083eecb74d68e
mijkami <dimitri.gaillot@gmail.com>
2019-07-09 09:40:28 +0200

    Isolate delete button, add role access

--- resources/views/pages/planningDelete.blade.php
+++ resources/views/pages/planningDelete.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.app')
 @section('content')
-<h3>Suppression de la participation à la sortie.</h3>
-<a href="{{ URL::previous() }}"><p><i class="fas fa-arrow-left"> Annuler / page précédente</i></p></a><br>
-<form  method="POST" action="{{ route('particips.destroy', $particip->id) }}">
-    {{ csrf_field() }}
-    {{ method_field('DELETE') }}
-    <button type="submit">Supprimer</button>
-    {{-- tODO possibilité d'ajouter une confirmation supplémentaire cf JS/CSS, etc --}}
-</form>
-
+@if (session('role')=='admin' or session('role')=='superadmin' or (session('role')=='membre' and session('id')==$particip->user_id))
+    <h3>Suppression de la participation à la sortie.</h3>
+    <a href="{{ URL::previous() }}"><p><i class="fas fa-arrow-left"> Annuler / page précédente</i></p></a><br>
+    <form  method="POST" action="{{ route('particips.destroy', $particip->id) }}">
+        {{ csrf_field() }}
+        {{ method_field('DELETE') }}
+        <button class="ml-4 p-2" type="submit">Supprimer</button>
+        {{-- tODO possibilité d'ajouter une confirmation supplémentaire cf JS/CSS, etc --}}
+    </form>
+@endif
 @stop
