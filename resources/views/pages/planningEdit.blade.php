e9e43026c5b01e954a83b0b9c696f0b2fbe6253e
mijkami <dimitri.gaillot@gmail.com>
2019-07-10 11:21:41 +0200

    styling planningEdit + responsive

--- resources/views/pages/planningEdit.blade.php
+++ resources/views/pages/planningEdit.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.app')
 @section('content')
-<h2>Modifier une participation</h2>
+@if (session('role')=='admin' or session('role')=='superadmin' or (session('role')=='membre' and session('id')==$particip->user_id))
+<h3>Modifier une participation</h3>
 <a href="{{ URL::previous() }}"><p><i class="fas fa-arrow-left"> Annuler / page précédente</i></p></a>
-
-<p>Participant : {{$particip->User->name.' '.$particip->User->firstname}}</p>
-<p>Date de la sortie : {{ Date::parse($particip->Sor->dat)->format('l j F') }}</p>
-
+<div class="row">
+    <h5 class="col-5 col-md-2 font-weight-bold">Participant</h5> 
+    <p class="col">{{$particip->User->name.' '.$particip->User->firstname}}</p>
+</div>
+<div class="row">
+    <h5 class="col-6 col-md-2 font-weight-bold">Date de la sortie</h5>
+    <p class="col">{{ Date::parse($particip->Sor->dat)->format('l j F') }}</p>
+</div>
 
 
 
@@ -14,21 +19,20 @@
     {{ method_field('PuT') }}
 
     @if (session('role')=='admin' OR session('role')=='superadmin')
+        <h4 class="font-weight-bold text-danger">Modification date (admin) :</h4>
+        <p>Une nouvelle date d'inscription implique un nouvel ordre de priorité dépendant de cette date à utiliser pour changer l'ordre des priorités.<br>
+        Exemple : encadrants, débutants dans une sortie encadrée, ...</p>
 
-        Une nouvelle date d'inscription implique un nouvel ordre de priorité dépendant de cette date à utiliser pour changer l'ordre des priorités,
-        par exemple : encadrants, débutants dans une sortie encadrée, ...<br>
 
-
-            <h4>Changer la date d'inscription</h4>
-            <input type="date" name="inscription"  value="{{ Date::parse($particip->inscription)->format('Y-m-d')}}"> {{ Date::parse($particip->inscription)->format('l j F') }}<br>
-
+        <h4 class="offset-1 text-danger">Changer la date d'inscription :</h4>
+        <div class="row mb-4 ml-3">
+            <input type="date" name="inscription" class="col-5 col-md-2  offset-1" value="{{ Date::parse($particip->inscription)->format('Y-m-d')}}"><p class="col mt-2">{{ Date::parse($particip->inscription)->format('l j F') }}</p>
+        </div>
     @endif
-    <h4>Modifier le commentaire du participant</h4>
-
-
-        <textarea rows="1" cols="60" maxlength="60" name="comment_particip" value="{{ $particip->comment_particip }}" class="mceNoEditor"> {{ $particip->comment_particip }}</textarea><br>
+    <h5 class="font-weight-bold">Modifier le commentaire</h5>
+        <textarea rows="2" cols="37" maxlength="60" name="comment_particip" value="{{ $particip->comment_particip }}" class="mceNoEditor"> {{ $particip->comment_particip }}</textarea><br>
         <button class="m-4 p-3 btn btn-success" type="submit">Enregistrer</button>
 </form>
-
+@endif
 @stop
 
