b254e30b66cd30cb91da885d951d8876d2a9eb0c
mijkami <dimitri.gaillot@gmail.com>
2019-07-24 14:06:28 +0200

    edit main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,24 +6,26 @@
         @include('includes.header')
 
         <main class="py-4">
-            @include('flash-message')
-            <div class="container">
-                <div class="panel panel-default">
-                    <div class="panel-heading"></div>
-                        <div class="panel-body">
-                            @yield('content')
+            <section>
+                @include('flash-message')
+                <div class="container">
+                    <div class="panel panel-default">
+                        <div class="panel-heading"></div>
+                            <div class="panel-body">
+                                @yield('content')
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </main>
-        <section class="footer-section">
-            <div class="container">
-                <div class="row p-2 my-5">
-                    @include('includes.footer')
+            </section>
+            <section class="footer-section">
+                <div class="container">
+                    <div class="row p-2 my-5">
+                        @include('includes.footer')
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </main>
     </div>
 </body>
 </html>
