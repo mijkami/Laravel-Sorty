9caf54ca2547277607c9368740a6bc6e9769377c
mijkami <dimitri.gaillot@gmail.com>
2019-07-25 14:54:14 +0200

    moving Parapangue link from navbar to title

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <h3>
-                    {{ config('app.name', 'Laravel') }}
+                    <a class="nav-link" href="http://www.parapangue.re/">Parapangue</a>
                 </h3>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -10,9 +10,6 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                             <a class="nav-link" href="http://www.parapangue.re/">Parapangue</a>
-                        </li>
                         <li class="nav-item">
                              <a class="nav-link" href="/">{{ __('Planning') }}</a>
                         </li>
