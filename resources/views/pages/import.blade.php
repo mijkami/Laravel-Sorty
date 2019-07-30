88ac84cc1c1b73ddf29e98eda12752a2caf4184a
mijkami <dimitri.gaillot@gmail.com>
2019-07-30 11:28:56 +0200

    styling import view

--- resources/views/pages/import.blade.php
+++ resources/views/pages/import.blade.php
@@ -2,13 +2,13 @@
 @section('content')
 <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
     @csrf
-    <label for="file">Sélectionner le fichier à importer (xls, ods, csv)</label>
-    <input type="file" name="file" class="form-control" >
+    <label for="file"><h3>Sélectionnez le fichier à importer (xls, ods, csv) :</h3></label>
+    <input type="file" name="file" class="form-control-file ml-4 mt-2" >
     <br>
-    <button class="btn btn-success">Import </button>
+    <button class="btn btn-success ml-3">Import</button>
     <?php
 
     ?>
-    <a class="btn btn-warning" href="{{ route('export') }}">Export </a>
+    <a class="btn btn-warning" href="{{ route('export') }}">Export</a>
 </form>
 @stop
