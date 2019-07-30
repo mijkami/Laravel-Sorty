c19ebc83fba58ed3b2435a302ec2a755ae97ae84
mijkami <dimitri.gaillot@gmail.com>
2019-07-30 11:31:35 +0200

    edit style import

--- resources/views/pages/import.blade.php
+++ resources/views/pages/import.blade.php
@@ -5,7 +5,7 @@
     <label for="file"><h3>Sélectionnez le fichier à importer (xls, ods, csv) :</h3></label>
     <input type="file" name="file" class="form-control-file ml-4 mt-2" >
     <br>
-    <button class="btn btn-success ml-3">Import</button>
+    <button class="btn btn-success mx-2">Import</button>
     <?php
 
     ?>
