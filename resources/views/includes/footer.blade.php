c5e692b821c50ed06b14cd6047d89bb82c0b7cda
mijkami <dimitri.gaillot@gmail.com>
2019-08-01 12:14:17 +0200

    editing footer style & content

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,55 @@
-
 {{-- #TODO : remplir footer (liens contact admin / association, etc) --}}
+{{-- <div id="col copyright text-right">© Copyright 2019 Association Parapangue</div>--}}
 
-<div id="col copyright text-right">© Copyright 2019 Association Parapangue</div>
+<section class="footer-section">
+		<div class="container">
+			<div class="footer-logo text-right">
+			</div>
+			<div class="row justify-content-between">
+				<div class="col-lg-3 col-md-4 col-sm-6 mb-n5">
+					<div class="footer-widget about-widget">
+						<h2>À propos</h2>
+                        <p>Le parachute de pente est dérivé du parachutisme et du vol libre en Haute Savoie le 27 juin 1978. <br>
+                            Il est ensuite arrivé en 1984 à la Réunion à la demande de parachutistes de Pierrefond.<br>
+                            L’activité est dite réservée aux "fêlés" avant sa démocratisation en 1988 !<br>
+                            </p>
+                            <p class="text-right"><a href="http://www.parapangue.re/wordpress/">En savoir plus !</a></p>
 
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-7">
+					<div class="footer-widget contact-widget">
+						<h2>Contacts</h2>
+						<div class="con-info">
+							<p class="h4">Parapangue Club-école</p>
+						</div>
+						<div class="con-info">
+							<p>Label CEFVL, Parapente n° 11590</p>
+                        </div>
+                        <div class="con-info">
+                            <span>Ad.</span>
+							<p>249 chemin mutel des indes 97436 Saint Leu</p>
+						</div>
+						<div class="con-info">
+							<span>Tél.</span>
+							<p>06 92 88 09 21</p>
+						</div>
+						{{-- <div class="con-info">
+							<span>E.</span>
+							<p>office@youremail.com</p>
+						</div> --}}
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-6 mb-5">
+					<a href="http://www.parapangue.re/"><img class="paraLogoFooter rounded bg-light mt-2" src="images/logo_parapangue_HD_square.png" alt=""></a>
+                </div>
+
+            </div>
+
+
+		</div>
+
+
+			</div>
+		</div>
+	</section>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,26 +5,20 @@
     <div id="app">
         @include('includes.header')
 
-        <main class="py-4">
+        <main class="py-5 mb-5">
             <section>
                 @include('flash-message')
                 <div class="container">
                     <div class="panel panel-default">
                         <div class="panel-heading"></div>
-                            <div class="panel-body">
+                            <div class="panel-body pb-5 mb-5">
                                 @yield('content')
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
-            <section class="footer-section">
-                <div class="container">
-                    <div class="row p-2 my-5">
-                        @include('includes.footer')
-                    </div>
-                </div>
-            </section>
+            @include('includes.footer')
         </main>
     </div>
 </body>
