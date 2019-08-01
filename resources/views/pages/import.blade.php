6a94a5fd7c819a4cebcfc54414fd51cf40fce1c2
mijkami <dimitri.gaillot@gmail.com>
2019-08-01 12:13:45 +0200

    editing import view style

--- resources/views/pages/import.blade.php
+++ resources/views/pages/import.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
+<form action="{{ route('import') }}" class="pb-5 mb-5" method="POST" enctype="multipart/form-data">
     @csrf
     <label for="file"><h3>Sélectionnez le fichier à importer (xls, ods, csv) :</h3></label>
     <input type="file" name="file" class="form-control-file ml-4 mt-2" >
