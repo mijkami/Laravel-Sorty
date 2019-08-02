fb9ddee329a83d08c995da71ebbcd43dba4782a4
mijkami <dimitri.gaillot@gmail.com>
2019-08-02 15:05:24 +0200

    edit style userDelete

--- resources/views/pages/userDelete.blade.php
+++ resources/views/pages/userDelete.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 @section('content')
 @if (session('role')=='superadmin')
-    <h2>Suppression de {{ $user->name }}</h2>
-    <a href="{{ URL::previous() }}"><p><i class="fas fa-arrow-left"> Annuler / page précédente</i></p></a>
+    <h3>{{ $user->name }} {{ $user->firstname }} : <span class="h2 font-weight-bold">suppression</span> de la fiche personelle</h3>
+    <a href="{{ URL::previous() }}"><h3><i class=" fas fa-arrow-left"> Annuler / page précédente</i></h3></a>
     <form  method="POST" action="{{ route('users.destroy', $user->id) }}">
         {{ csrf_field() }}
         {{ method_field('DELETE') }}
+        <h2 class="font-weight-bold text-danger mt-5">Attention ! Tout donnée supprimée est irrécupérable.</h2>
         <button class="m-4 p-2 btn btn-danger" type="submit">Supprimer</button>
     </form>
 @endif
