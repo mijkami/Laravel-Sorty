2eec53050ec364921c003bc8c6ccf9eb64a738cb
mijkami <dimitri.gaillot@gmail.com>
2019-08-05 15:53:58 +0200

    showUser large style edit

--- resources/views/includes/showUser.blade.php
+++ resources/views/includes/showUser.blade.php
@@ -1,5 +1,5 @@
 <div class="row justify-content-start no-gutters mt-3 mt-md-0 {{$color[$user->statut]}}">
-    <div class="col-3 col-md-2 h4">
+    <div class="col-3 col-md-2 col-lg-1 h4">
             <a href="/users/{{ $user['id'] }}/edit" aria-label="Editer"><i class="fas fa-user-edit" title="Modifier '{{ $user['name'] }} {{ $user['firstname'] }}'"></i></a> / <a href="/users/{{ $user['id'] }}/destroy" aria-laber="Enlever"><i class="fas fa-user-times" title="Enlever '{{ $user['name'] }} {{ $user['firstname'] }}''"></i>
             </a>
     </div>
