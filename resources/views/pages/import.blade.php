2fd260e0e0494198ff7e43463cdfcb482f8026dc
mijkami <dimitri.gaillot@gmail.com>
2019-08-05 10:32:16 +0200

    import view : slight style change

--- resources/views/pages/import.blade.php
+++ resources/views/pages/import.blade.php
@@ -3,8 +3,9 @@
 <h2>Import / Export données membres de Parapangue</h2>
 <div class="container p-2 ml-3">
     <div class="row justify-content-start">
-        <div class="font-weight-bold col-4 col-md-1 px-2">Import :</div><div class="col-8 col-md-6 mb-2">Entêtes de colonnes (sans accent) : <b>'nom', 'prenom', 'tel' , 'email'</b></div>
-            <div class="col-8 col-md-6 offset-4 offset-md-1">Format <b>xls, xslx, ods</b> :
+        <div class="font-weight-bold col-3 col-md-1 px-2">Import :</div>
+        <div class="col-8 col-md-9 mb-2">Entêtes de colonnes (sans accent) : <b>'nom', 'prenom', 'tel' , 'email'</b></div>
+            <div class="col-8 col-md-6 offset-3 offset-md-1">Format <b>xls, xslx, ods</b> :
                 <ul>
                     <li>1 seule feuille</li>
                     <li>pas de case vide dans la colonne 'email'</li>
@@ -12,10 +13,10 @@
             </div>
         </div>
     <div class="row justify-content-start mt-2">
-        <div class="font-weight-bold col-4 col-md-1 px-2">Export :</div><p class="col-8 col-md-6">Format <b>xlsx</b>.</p>
+        <div class="font-weight-bold col-3 col-md-1 px-2">Export :</div><p class="col-8 col-md-6">Format <b>xlsx</b>.</p>
     </div>
 </div>
-<form action="{{ route('import') }}" class="col-12 col-md-7 border my-4" method="POST" enctype="multipart/form-data">
+<form action="{{ route('import') }}" class="col-12 col-lg-7 col-md-6 border my-4" method="POST" enctype="multipart/form-data">
     @csrf
     <div class="p-4">
         <label for="file"><h4>Sélectionnez le fichier à importer (xls, ods, csv) :</h4></label>
