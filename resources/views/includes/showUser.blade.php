d23c52d44288dc389a33fe7eb4f116a320608583
mijkami <dimitri.gaillot@gmail.com>
2019-08-05 14:52:58 +0200

    UX : adding alt text to edit / delete user links

--- resources/views/includes/showUser.blade.php
+++ resources/views/includes/showUser.blade.php
@@ -1,6 +1,6 @@
 <div class="row justify-content-start no-gutters mt-3 mt-md-0 {{$color[$user->statut]}}">
     <div class="col-2 col-md-1">
-            <a href="/users/{{ $user['id'] }}/edit"><i class="fas fa-user-edit"></i></a> / <a href="/users/{{ $user['id'] }}/destroy"><i class="fas fa-user-times"></i>
+            <a href="/users/{{ $user['id'] }}/edit" aria-label="Editer"><i class="fas fa-user-edit" title="Modifier '{{ $user['name'] }} {{ $user['firstname'] }}'"></i></a> / <a href="/users/{{ $user['id'] }}/destroy" aria-laber="Enlever"><i class="fas fa-user-times" title="Enlever '{{ $user['name'] }} {{ $user['firstname'] }}''"></i>
             </a>
     </div>
     <div class="col-5 col-md-2">{{ $user['name'] }}</div>
