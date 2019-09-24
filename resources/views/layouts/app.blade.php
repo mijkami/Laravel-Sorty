0fc127157b10a66f0acb4384d7d15a3865d0b519
mijkami <dimitri.gaillot@gmail.com>
2019-09-24 12:08:44 +0200

    indent body in main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     @include('includes.head')
-<body>
-    <div id="app">
-        @include('includes.header')
+    <body>
+        <div id="app">
+            @include('includes.header')
 
-        <main>
-            <section>
-                @include('flash-message')
-                <div class="container py-5 mb-5">
-                    <div class="panel panel-default">
-                        <div class="panel-heading"></div>
-                            <div class="panel-body pb-5 mb-5">
-                                @yield('content')
+            <main>
+                <section>
+                    @include('flash-message')
+                    <div class="container py-5 mb-5">
+                        <div class="panel panel-default">
+                            <div class="panel-heading"></div>
+                                <div class="panel-body pb-5 mb-5">
+                                    @yield('content')
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
-            @include('includes.footer')
-        </main>
-    </div>
-</body>
+                </section>
+                @include('includes.footer')
+            </main>
+        </div>
+    </body>
 </html>
